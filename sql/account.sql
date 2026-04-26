0fe23f30dff925fcfd07cc69e1c0a9f9885cc858
qi.wangwq <qi.wangwq@shopee.com>
2026-04-26 17:01:00 +0800

    sql数据

--- sql/account.sql
+++ sql/account.sql
@@ -1,44 +1,4 @@
-/*
- Navicat Premium Dump SQL
-
- Source Server         : DuQnng
- Source Server Type    : MySQL
- Source Server Version : 90200 (9.2.0)
- Source Host           : localhost:3306
- Source Schema         : tlias
-
- Target Server Type    : MySQL
- Target Server Version : 90200 (9.2.0)
- File Encoding         : 65001
-
- Date: 22/03/2026 16:48:53
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for account
--- ----------------------------
-DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account`  (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '账户ID',
-  `user_id` bigint NOT NULL COMMENT '用户ID，对应登录用户',
-  `account_no` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '账户号',
-  `account_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '储蓄卡' COMMENT '账户类型',
-  `balance` decimal(12, 2) NULL DEFAULT 0.00 COMMENT '账户余额',
-  `status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '正常' COMMENT '账户状态',
-  `create_time` datetime NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '银行账户表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of account
--- ----------------------------
-INSERT INTO `account` VALUES (1, 1, '6222000012345678', '储蓄卡', 90000.67, '正常', '2026-01-11 17:20:48', '2026-03-22 16:14:20');
-INSERT INTO `account` VALUES (2, 2, '6222000098765432', '储蓄卡', 9876.50, '正常', '2026-01-11 17:25:12', '2026-01-11 17:25:12');
-INSERT INTO `account` VALUES (3, 3, '6222000043210987', '信用卡', 15000.00, '正常', '2026-01-11 17:30:05', '2026-03-22 16:14:20');
-INSERT INTO `account` VALUES (4, 4, '6222000076543210', '储蓄卡', 32273.00, '正常', '2026-01-11 17:35:20', '2026-03-17 14:19:40');
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO tlias.account (id, user_id, account_no, account_type, balance, status, create_time, update_time) VALUES (1, 1, '6222000012345678', '储蓄卡', 90700.67, '正常', '2026-01-11 17:20:48', '2026-04-23 12:03:02');
+INSERT INTO tlias.account (id, user_id, account_no, account_type, balance, status, create_time, update_time) VALUES (2, 2, '6222000098765432', '储蓄卡', 11276.50, '正常', '2026-01-11 17:25:12', '2026-04-23 12:03:02');
+INSERT INTO tlias.account (id, user_id, account_no, account_type, balance, status, create_time, update_time) VALUES (3, 3, '6222000043210987', '信用卡', 9900.00, '正常', '2026-01-11 17:30:05', '2026-04-23 11:48:09');
+INSERT INTO tlias.account (id, user_id, account_no, account_type, balance, status, create_time, update_time) VALUES (4, 3, '6222000076543210', '储蓄卡', 35273.00, '正常', '2026-01-11 17:35:20', '2026-04-23 11:06:08');
--- sql/transaction_record.sql
+++ sql/transaction_record.sql
@@ -1,69 +1,73 @@
-/*
- Navicat Premium Dump SQL
+CREATE TABLE `transaction_record` (
+                                      `id` bigint NOT NULL AUTO_INCREMENT,
+                                      `account_id` bigint NOT NULL,
+                                      `counterparty_account_no` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                                      `type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+                                      `amount` decimal(12,2) NOT NULL,
+                                      `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                                      `trade_time` datetime DEFAULT CURRENT_TIMESTAMP,
+                                      PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC
 
- Source Server         : DuQnng
- Source Server Type    : MySQL
- Source Server Version : 90200 (9.2.0)
- Source Host           : localhost:3306
- Source Schema         : tlias
 
- Target Server Type    : MySQL
- Target Server Version : 90200 (9.2.0)
- File Encoding         : 65001
 
- Date: 22/03/2026 16:49:13
-*/
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (1, 1, '6222000098765432', '收入', 5000.00, '工资', '2026-01-11 18:47:17');
 
--- ----------------------------
--- Table structure for transaction_record
--- ----------------------------
-DROP TABLE IF EXISTS `transaction_record`;
-CREATE TABLE `transaction_record`  (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `account_id` bigint NOT NULL,
-  `counterparty_account_no` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `amount` decimal(12, 2) NOT NULL,
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `trade_time` datetime NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 35 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of transaction_record
--- ----------------------------
-INSERT INTO `transaction_record` VALUES (1, 1, '6222000098765432', '收入', 5000.00, '工资', '2026-01-11 18:47:17');
-INSERT INTO `transaction_record` VALUES (2, 1, '6222000076543210', '支出', -200.00, '购物', '2026-01-11 18:47:17');
-INSERT INTO `transaction_record` VALUES (7, 1, '6222000076543210', '支出', -2000.00, '有钱！大把挥霍！', '2026-01-11 19:31:54');
-INSERT INTO `transaction_record` VALUES (8, 4, '6222000012345678', '收入', 2000.00, '有钱！大把挥霍！', '2026-01-11 19:31:54');
-INSERT INTO `transaction_record` VALUES (9, 1, '6222000076543210', '支出', -111.00, '22222', '2026-01-17 09:32:14');
-INSERT INTO `transaction_record` VALUES (10, 4, '6222000012345678', '收入', 111.00, '22222', '2026-01-17 09:32:14');
-INSERT INTO `transaction_record` VALUES (11, 1, '6222000076543210', '支出', -200.00, '111', '2026-01-17 10:09:19');
-INSERT INTO `transaction_record` VALUES (12, 4, '6222000012345678', '收入', 200.00, '111', '2026-01-17 10:09:19');
-INSERT INTO `transaction_record` VALUES (13, 1, '6222000076543210', '支出', -123.00, '转账逻辑测试', '2026-01-17 10:21:08');
-INSERT INTO `transaction_record` VALUES (14, 4, '6222000012345678', '收入', 123.00, '转账逻辑测试', '2026-01-17 10:21:08');
-INSERT INTO `transaction_record` VALUES (15, 1, '6222000076543210', '支出', -123.00, '转账逻辑测试', '2026-01-17 10:21:41');
-INSERT INTO `transaction_record` VALUES (16, 4, '6222000012345678', '收入', 123.00, '转账逻辑测试', '2026-01-17 10:21:41');
-INSERT INTO `transaction_record` VALUES (17, 1, '6222000076543210', '支出', -123.00, '转账逻辑测试222', '2026-01-17 10:21:49');
-INSERT INTO `transaction_record` VALUES (18, 4, '6222000012345678', '收入', 123.00, '转账逻辑测试222', '2026-01-17 10:21:49');
-INSERT INTO `transaction_record` VALUES (19, 1, '6222000076543210', '支出', -123.00, '逻辑测试第二次', '2026-01-17 10:25:32');
-INSERT INTO `transaction_record` VALUES (20, 4, '6222000012345678', '收入', 123.00, '逻辑测试第二次', '2026-01-17 10:25:32');
-INSERT INTO `transaction_record` VALUES (21, 1, '6222000076543210', '支出', -60.00, '测试逻辑第3次', '2026-01-17 10:27:54');
-INSERT INTO `transaction_record` VALUES (22, 4, '6222000012345678', '收入', 60.00, '测试逻辑第3次', '2026-01-17 10:27:54');
-INSERT INTO `transaction_record` VALUES (23, 1, '6222000076543210', '支出', -73.00, '', '2026-03-08 22:43:36');
-INSERT INTO `transaction_record` VALUES (24, 4, '6222000012345678', '收入', 73.00, '', '2026-03-08 22:43:36');
-INSERT INTO `transaction_record` VALUES (25, 1, '6222000076543210', '支出', -300.00, '', '2026-03-08 23:09:09');
-INSERT INTO `transaction_record` VALUES (26, 4, '6222000012345678', '收入', 300.00, '', '2026-03-08 23:09:09');
-INSERT INTO `transaction_record` VALUES (27, 1, '6222000076543210', '支出', -250.00, '', '2026-03-09 00:02:27');
-INSERT INTO `transaction_record` VALUES (28, 4, '6222000012345678', '收入', 250.00, '', '2026-03-09 00:02:27');
-INSERT INTO `transaction_record` VALUES (29, 1, '6222000076543210', '支出', -300.00, '测试5566', '2026-03-17 12:58:06');
-INSERT INTO `transaction_record` VALUES (30, 4, '6222000012345678', '收入', 300.00, '测试5566', '2026-03-17 12:58:06');
-INSERT INTO `transaction_record` VALUES (31, 1, '6222000076543210', '支出', -487.00, '测试3333', '2026-03-17 14:19:40');
-INSERT INTO `transaction_record` VALUES (32, 4, '6222000012345678', '收入', 487.00, '测试3333', '2026-03-17 14:19:40');
-INSERT INTO `transaction_record` VALUES (33, 1, '6222000043210987', '支出', -10000.00, '风险测试', '2026-03-22 16:14:21');
-INSERT INTO `transaction_record` VALUES (34, 3, '6222000012345678', '收入', 10000.00, '风险测试', '2026-03-22 16:14:21');
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (2, 1, '6222000076543210', '支出', -200.00, '购物', '2026-01-11 18:47:17');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (7, 1, '6222000076543210', '支出', -2000.00, '有钱！大把挥霍！', '2026-01-11 19:31:54');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (8, 4, '6222000012345678', '收入', 2000.00, '有钱！大把挥霍！', '2026-01-11 19:31:54');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (9, 1, '6222000076543210', '支出', -111.00, '22222', '2026-01-17 09:32:14');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (10, 4, '6222000012345678', '收入', 111.00, '22222', '2026-01-17 09:32:14');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (11, 1, '6222000076543210', '支出', -200.00, '111', '2026-01-17 10:09:19');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (12, 4, '6222000012345678', '收入', 200.00, '111', '2026-01-17 10:09:19');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (13, 1, '6222000076543210', '支出', -123.00, '转账逻辑测试', '2026-01-17 10:21:08');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (14, 4, '6222000012345678', '收入', 123.00, '转账逻辑测试', '2026-01-17 10:21:08');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (15, 1, '6222000076543210', '支出', -123.00, '转账逻辑测试', '2026-01-17 10:21:41');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (16, 4, '6222000012345678', '收入', 123.00, '转账逻辑测试', '2026-01-17 10:21:41');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (17, 1, '6222000076543210', '支出', -123.00, '转账逻辑测试222', '2026-01-17 10:21:49');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (18, 4, '6222000012345678', '收入', 123.00, '转账逻辑测试222', '2026-01-17 10:21:49');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (19, 1, '6222000076543210', '支出', -123.00, '逻辑测试第二次', '2026-01-17 10:25:32');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (20, 4, '6222000012345678', '收入', 123.00, '逻辑测试第二次', '2026-01-17 10:25:32');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (21, 1, '6222000076543210', '支出', -60.00, '测试逻辑第3次', '2026-01-17 10:27:54');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (22, 4, '6222000012345678', '收入', 60.00, '测试逻辑第3次', '2026-01-17 10:27:54');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (23, 1, '6222000076543210', '支出', -73.00, '', '2026-03-08 22:43:36');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (24, 4, '6222000012345678', '收入', 73.00, '', '2026-03-08 22:43:36');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (25, 1, '6222000076543210', '支出', -300.00, '', '2026-03-08 23:09:09');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (26, 4, '6222000012345678', '收入', 300.00, '', '2026-03-08 23:09:09');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (27, 1, '6222000076543210', '支出', -250.00, '', '2026-03-09 00:02:27');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (28, 4, '6222000012345678', '收入', 250.00, '', '2026-03-09 00:02:27');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (29, 1, '6222000076543210', '支出', -300.00, '测试5566', '2026-03-17 12:58:06');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (30, 4, '6222000012345678', '收入', 300.00, '测试5566', '2026-03-17 12:58:06');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (31, 1, '6222000076543210', '支出', -487.00, '测试3333', '2026-03-17 14:19:40');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (32, 4, '6222000012345678', '收入', 487.00, '测试3333', '2026-03-17 14:19:40');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (33, 1, '6222000043210987', '支出', -10000.00, '风险测试', '2026-03-22 16:14:21');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (34, 3, '6222000012345678', '收入', 10000.00, '风险测试', '2026-03-22 16:14:21');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (35, 3, '6222000012345678', '支出', -10000.00, '大额支出', '2026-04-07 14:37:33');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (36, 1, '6222000043210987', '收入', 10000.00, '大额支出', '2026-04-07 14:37:33');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (37, 3, '6222000012345678', '支出', -100.00, '小额', '2026-04-07 14:37:42');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (38, 1, '6222000043210987', '收入', 100.00, '小额', '2026-04-07 14:37:42');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (39, 3, '6222000012345678', '支出', -1000.00, '小额', '2026-04-07 14:37:52');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (40, 1, '6222000043210987', '收入', 1000.00, '小额', '2026-04-07 14:37:52');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (41, 1, '6222000098765432', '支出', -300.00, '弟弟', '2026-04-23 10:55:17');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (42, 2, '6222000012345678', '收入', 300.00, '弟弟', '2026-04-23 10:55:17');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (43, 1, '6222000098765432', '支出', -300.00, '弟弟的午餐费', '2026-04-21 10:55:38');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (44, 2, '6222000012345678', '收入', 300.00, '弟弟的午餐费', '2026-04-21 10:55:38');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (45, 1, '6222000098765432', '支出', -100.00, '晚餐费', '2026-04-17 11:04:47');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (46, 2, '6222000012345678', '收入', 100.00, '晚餐费', '2026-04-17 11:04:47');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (47, 1, '6222000043210987', '支出', -3000.00, '团建费', '2026-03-23 11:05:44');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (48, 3, '6222000012345678', '收入', 3000.00, '团建费', '2026-03-23 11:05:44');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (49, 1, '6222000076543210', '支出', -3000.00, '', '2026-04-23 11:06:08');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (50, 4, '6222000012345678', '收入', 3000.00, '', '2026-04-23 11:06:08');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (51, 1, '6222000098765432', '支出', -100.00, '晚餐费', '2026-04-23 11:22:44');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (52, 2, '6222000012345678', '收入', 100.00, '晚餐费', '2026-04-23 11:22:44');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (53, 1, '6222000098765432', '支出', -300.00, '午餐费', '2026-04-23 11:44:56');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (54, 2, '6222000012345678', '收入', 300.00, '午餐费', '2026-04-23 11:44:56');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (55, 1, '6222000098765432', '支出', -100.00, '晚餐费', '2026-04-23 11:45:23');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (56, 2, '6222000012345678', '收入', 100.00, '晚餐费', '2026-04-23 11:45:23');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (57, 1, '6222000098765432', '支出', -100.00, '晚餐费', '2026-04-23 11:46:52');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (58, 2, '6222000012345678', '收入', 100.00, '晚餐费', '2026-04-23 11:46:52');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (59, 1, '6222000043210987', '支出', -3000.00, '团建费', '2026-04-23 11:48:10');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (60, 3, '6222000012345678', '收入', 3000.00, '团建费', '2026-04-23 11:48:10');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (61, 1, '6222000098765432', '支出', -100.00, '晚餐费', '2026-04-23 12:03:03');
+INSERT INTO tlias.transaction_record (id, account_id, counterparty_account_no, type, amount, description, trade_time) VALUES (62, 2, '6222000012345678', '收入', 100.00, '晚餐费', '2026-04-23 12:03:03');
